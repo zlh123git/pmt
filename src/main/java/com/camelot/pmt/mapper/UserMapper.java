c218a0f18ef87c0f6f22caf8f9758457dc22effd
lvp <lvpeng@camelotchina.com>
2018-03-19 17:27:04 +0800

    test

---
+++ src/main/java/com/camelot/pmt/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.camelot.pmt.mapper;
+
+import com.camelot.pmt.mode.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer userId);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer userId);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
